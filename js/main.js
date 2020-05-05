ac2104bca402be583e96cd8dc5229631731d5ac4
Yuriy Zudikhin <uzudihin@gmail.com>
2020-05-05 19:55:21 +0300

    btn footer

--- js/main.js
+++ js/main.js
@@ -40,10 +40,56 @@ $(document).ready(function () {
     }
   });
 
+  $(".navigation__content-cart").click(function () {
+    $(".header__mobile-cart-content").toggleClass("active");
+    if ($(".header__mobile-cart-content").hasClass("active")) {
+      $(".header__fake-modal").css("display", "block");
+      $("body").css("overflow", "hidden");
+    } else {
+      $(".header__fake-modal").css("display", "none");
+      $("body").css("overflow", "auto");
+    }
+  });
+
   $(".header__mobile-cart-content .cart__close").click(function () {
     $(".header__mobile-cart-content").removeClass("active");
     $(".header__fake-modal").css("display", "none");
     $("body").removeClass("notouch");
     $("body").css("overflow", "auto");
   });
+
+  $(".main-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+  });
+
+  $(".quantity-arrow-minus").click(function () {
+    var currentNum = $(this).siblings().first();
+    console.log(currentNum.val());
+    // if (currentNum.val() > 1) {
+    //   currentNum.val(+currentNum.val() - 1);
+    // }
+    // console.log(currentNum);
+    // console.log(currentNum.val());
+  });
+
+  $(".quantity-arrow-plus").click(function () {
+    var currentNum = $(this).siblings().first();
+    console.log(currentNum);
+
+    // currentNum.val(+currentNum + 1);
+    // console.log(currentNum);
+    // console.log(currentNum.val());
+  });
+
+  // function quantityMinus() {
+  //   if (quantityNum.val() > 1) {
+  //     quantityNum.val(+quantityNum.val() - 1);
+  //   }
+  // }
+
+  // function quantityPlus() {
+  //   quantityNum.val(+quantityNum.val() + 1);
+  // }
 });
